97aef962565d7a9e1ed640b7417b93daa246e63d
JosephAwuku33 <33jaykay@gmail.com>
2026-07-22 18:50:03 +0000

    commented the page

--- tickethub-platform-frontend/src/pages/Dashboard/Organizer/AnalyticsOverview.tsx
+++ tickethub-platform-frontend/src/pages/Dashboard/Organizer/AnalyticsOverview.tsx
@@ -1,5 +1,5 @@
-import AnalyticsOverviewSection from "@/components/sections/Dashboard/Organizer/Analytics/AnalyticsOverview";
+// import AnalyticsOverviewSection from "@/components/sections/Dashboard/Organizer/Analytics/AnalyticsOverview";
 
-export default function AnalyticsOverview() {
-  return <AnalyticsOverviewSection />;
-}
+// export default function AnalyticsOverview() {
+//   return <AnalyticsOverviewSection />;
+// }
